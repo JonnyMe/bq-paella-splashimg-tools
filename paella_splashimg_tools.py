fcd66c7865a0bf59be5d8ff69bf9565b99e256e5
jonathan <jonathan.albrieux@gmail.com>
2020-07-01 21:11:33 +0200

    clean up

--- paella_splashimg_tools.py
+++ paella_splashimg_tools.py
@@ -34,7 +34,6 @@ def determine_quartet_identity(byte_quartet, prev_byte_quartet):
 def get_splash_pictures_coordinates(filename):
 	pictures_coordinates = list()
 	picture_coordinates = list()
-
 	byte_quartet = list()
 	prev_byte_quartet = list()
 
@@ -72,17 +71,15 @@ def quartet_to_pixels(byte_quartet, coords, img_matrix):
 			y += 1
 	return (x,y)
 
-def extract_pictures(filename):
+def extract_splash(filename):
+	f = open(filename, 'rb')
+	fsize = os.path.getsize(filename)
 	pictures_coordinates = get_splash_pictures_coordinates(filename)
 
 	for picture_coordinates in pictures_coordinates:
-		f = open(filename, 'rb')
-		fsize = os.path.getsize(filename)
-		byte_quartet = list()
-
 		img = Image.new('RGB', (BQ_AQUARIS_X5_SCREEN_W, BQ_AQUARIS_X5_SCREEN_H))
 		img_matrix = img.load()
-
+		byte_quartet = list()
 		coords = (0, 0)
 
 		for index in range(picture_coordinates[0], picture_coordinates[1], 4):
@@ -93,46 +90,37 @@ def extract_pictures(filename):
 		img.save('splash_%d.bmp' % index)
 	f.close()
 
-def rgb_to_brgle_bytes(picture):
-	im = Image.open(picture)
+def append_pixel(hex_image, pixel):
+	pixel_bgr = pixel[::-1]
 
+	if len(hex_image) < 4:
+		hex_image.extend(pixel_bgr)
+		hex_image.append(1)
+		return
+
+	if hex_image[-4:-1] == list(pixel_bgr):
+		if hex_image[-1] < 255:
+			hex_image[-1] += 1
+		else:
+			hex_image.extend(pixel_bgr)
+			hex_image.append(1)
+	else:
+		hex_image.extend(pixel_bgr)
+		hex_image.append(1)
+
+def picture_to_bgr_raw(picture):
+	im = Image.open(picture)
 	img_matrix = im.load()
 	hex_image = list()
 
-	current_pixel = [0] * 3
-	previous_pixel = list()
-	pixel_quartet = [0] * 4
+	for y in range(0, BQ_AQUARIS_X5_SCREEN_H):
+		for x in range(0, BQ_AQUARIS_X5_SCREEN_W):
+			append_pixel(hex_image, img_matrix[x,y])
 
-	for y in range(BQ_AQUARIS_X5_SCREEN_H):
-		for x in range(BQ_AQUARIS_X5_SCREEN_W):
-			current_pixel[0] = img_matrix[x,y][0]
-			current_pixel[1] = img_matrix[x,y][1]
-			current_pixel[2] = img_matrix[x,y][2]
-			
-			if current_pixel == previous_pixel:	
-				pixel_quartet[3] += 1
-				if pixel_quartet[3] == 255:
-					hex_image.extend(pixel_quartet)
-					pixel_quartet[3] = 0
-				elif y == BQ_AQUARIS_X5_SCREEN_H - 1 and x == BQ_AQUARIS_X5_SCREEN_W - 1:
-					hex_image.extend(pixel_quartet)
-			else:
-				if len(previous_pixel) > 0:
-					hex_image.extend(pixel_quartet)
-				pixel_quartet[0] = current_pixel[2]
-				pixel_quartet[1] = current_pixel[1]
-				pixel_quartet[2] = current_pixel[0]
-				pixel_quartet[3] = 1
-
-				if y == BQ_AQUARIS_X5_SCREEN_H - 1 and x == BQ_AQUARIS_X5_SCREEN_W - 1:
-					hex_image.extend(pixel_quartet)
-
-			previous_pixel = list(current_pixel)
-
+	print(hex_image)
 	return hex_image
 
 def create_splash(new_splashimg, splash_picture, fastboot_picture):
-
 	splash_picture_matrix = list()
 	fastboot_picture_matrix = list()
 
@@ -146,18 +134,22 @@ def create_splash(new_splashimg, splash_picture, fastboot_picture):
 	splash_header.close()
 
 	for index in range(FILEHEADER_LENGTH, OFFSET_SPLASH):
-		f_out.write((0).to_bytes(1, sys.byteorder))
+		f_out.write((0).to_bytes(1, 'little'))
 
 	if splash_picture:
-		splash_picture_matrix = rgb_to_brgle_bytes(splash_picture)
+		splash_picture_matrix = picture_to_bgr_raw(splash_picture)
 		splash_picture_size = len(splash_picture_matrix)
 
 		if splash_picture_size > SPLASH_MAX_SIZE:
-			print("%s too big (actual:%d, max:%d), abort" % (splash_picture, splash_picture_size, SPLASH_MAX_SIZE))
+			print("%s too big (actual:%d, max:%d), abort" % (
+				splash_picture,
+				splash_picture_size,
+				SPLASH_MAX_SIZE
+			))
 			exit()
 
 		for index in range(0, splash_picture_size):
-			f_out.write(splash_picture_matrix[index].to_bytes(1, sys.byteorder))
+			f_out.write(splash_picture_matrix[index].to_bytes(1, 'little'))
 	else:
 		splash_picture = open('splash_picture', 'rb')
 		splash_picture_size = os.path.getsize('splash_picture')
@@ -166,18 +158,22 @@ def create_splash(new_splashimg, splash_picture, fastboot_picture):
 		splash_picture.close()
 
 	for index in range(OFFSET_SPLASH + splash_picture_size, OFFSET_BOOTLOADER):
-		f_out.write((0).to_bytes(1, sys.byteorder))
+		f_out.write((0).to_bytes(1, 'little'))
 
 	if fastboot_picture:
-		fastboot_picture_matrix = rgb_to_brgle_bytes(fastboot_picture)
+		fastboot_picture_matrix = picture_to_bgr_raw(fastboot_picture)
 		fastboot_picture_size = len(fastboot_picture_matrix)
 
 		if fastboot_picture_size > BOOTLOADER_MAX_SIZE:
-			print("%s too big (actual:%d, max:%d), abort" % (fastboot_picture, fastboot_picture_size, BOOTLOADER_MAX_SIZE))
+			print("%s too big (actual:%d, max:%d), abort" % (
+				fastboot_picture,
+				fastboot_picture_size,
+				BOOTLOADER_MAX_SIZE
+			))
 			exit()
 		
 		for index in range(0, fastboot_picture_size):
-			f_out.write(fastboot_picture_matrix[index].to_bytes(1, sys.byteorder))
+			f_out.write(fastboot_picture_matrix[index].to_bytes(1, 'little'))
 	else:
 		fastboot_picture = open('fastboot_picture', 'rb')
 		fastboot_picture_size = os.path.getsize('fastboot_picture')
@@ -186,39 +182,47 @@ def create_splash(new_splashimg, splash_picture, fastboot_picture):
 		fastboot_picture.close()
 
 	for index in range(OFFSET_BOOTLOADER + fastboot_picture_size, SPLASHIMG_SIZE):
-			f_out.write((0).to_bytes(1, sys.byteorder))
+			f_out.write((0).to_bytes(1, 'little'))
 
 	f_out.close()
 
-def main(argv):
+def analyze_splash(target_splash):
+	pictures_coordinates = get_splash_pictures_coordinates(target_splash)
+	print(pictures_coordinates)
 
+def main(argv):
 	action = ''
-	new_splash = ''
+	target_splash = ''
 	splash_picture = False
 	fastboot_picture = False
 
 	try:
-		opts, args = getopt.getopt(argv,"a:e:co:s:f:")
+		opts, args = getopt.getopt(argv,"a:e:c:s:f:")
 	except getopt.GetoptError:
 		sys.exit(2)
 	
 	for opt, arg in opts:
 		if opt in ("-a"):
-			pictures_coordinates = get_splash_pictures_coordinates(arg)
-			print(pictures_coordinates)
+			action = analyze_splash
+			target_splash = arg
 		elif opt in ("-e"):
-			extract_pictures(arg)
+			action = extract_splash
+			target_splash = arg
 		elif opt in ("-c"):
 			action = create_splash
-		elif opt in ("-o"):
-			new_splash = arg
+			target_splash = arg
 		elif opt in ("-s"):
 			splash_picture = arg
 		elif opt in ("-f"):
 			fastboot_picture = arg
 
-	if action == create_splash and new_splash != '':
-		create_splash(new_splash, splash_picture, fastboot_picture)
+	if len(target_splash) == 0:
+		return
+
+	if action == create_splash:
+		action(target_splash, splash_picture, fastboot_picture)
+	elif action == extract_splash or action == analyze_splash:
+		action(target_splash)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
